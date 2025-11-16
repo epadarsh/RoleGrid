1c96d9df194b58a27c76e2ca66f3974b5031e9b7
adarsh.ep <adarsh.ep@fingent.com>
2025-11-16 23:49:36 +0530

    Creates Internal product listing route

--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,16 @@ use App\Http\Controllers\Admin\ProductController as AdminProductController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// --- Protected Routes (Sanctum) ---
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']); // User Dashboard Data
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::get('/products', [App\Http\Controllers\ProductController::class, 'index']);
 
-    // --- Admin Routes (Sanctum + Admin Middleware) ---
+
     Route::middleware('admin')->prefix('admin')->group(function () {
         // User Management (Admin CRUD)
         Route::apiResource('users', AdminUserController::class)->only(['index', 'store', 'update', 'destroy']);
-
         // Product Management (Internal CRUD)
         Route::apiResource('products', AdminProductController::class);
     });
